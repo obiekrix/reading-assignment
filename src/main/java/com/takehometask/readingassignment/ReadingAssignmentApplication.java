f03548a69711cf049251a741fc1b38e380675614
Christian Oliver <obiekrix@gmail.com>
2026-07-03 00:10:08 +0100

    Initial commit of project structure

---
+++ src/main/java/com/takehometask/readingassignment/ReadingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.takehometask.readingassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/takehometask/readingassignment/ReadingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takehometask.readingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
