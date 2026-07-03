9f90d674bb9537ed1846254c9e4617085e4108da
Christian Oliver <obiekrix@gmail.com>
2026-07-03 23:53:10 +0100

    Update CORS configuration and enable H2 console for remote access in development
    - Add support for separate local and production CORS origins.
    - Enable `web-allow-others` setting for the H2 console with a security warning.

--- src/main/java/com/takehometask/readingassignment/config/CorsConfig.java
+++ src/main/java/com/takehometask/readingassignment/config/CorsConfig.java
@@ -7,13 +7,18 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origin:http://localhost:4200}")
+    @Value("${app.cors.allowed-local-origin:http://localhost:4200}")
     private String allowedOrigin;
+
+    @Value("${app.cors.allowed-prod-origin:https://reading-assignment-ui.vercel.app}")
+    private String allowedProdOrigin;
+
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigin)
+                .allowedOrigins(allowedOrigin, allowedProdOrigin)
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
